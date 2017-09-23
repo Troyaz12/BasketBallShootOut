5df1bdde12ff117dfdb07289c61f5dae15299a51
Troy Gugler <guglert80@gmail.com>
2017-09-23 10:22:04 -0700

    feat: Set background color to black

--- core/src/com/tegprogramming/basketballshootout/MyGdxGame.java
+++ core/src/com/tegprogramming/basketballshootout/MyGdxGame.java
@@ -56,7 +56,7 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor {
 	}
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1,1,1,1);
+		Gdx.gl.glClearColor(0,0,0,0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 
